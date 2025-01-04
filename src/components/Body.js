4ee1a38375943c31b23798272e52fd5c04db7ddf
adwaithpvk45 <pvkadwaith1001@gmail.com>
2025-01-04 18:57:32 +0530

    fetch method used to fetch data from data.json file

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,101 +1,23 @@
 import Products from "./Products"
-import card1 from '../images/card1.png'
-import card2 from '../images/card2.png'
-import card3 from '../images/card3.png'
-import card4 from '../images/cardm4.png'
-import card5 from '../images/card5.png'
-import card6 from '../images/card6.png'
+// import card1 from '../images/card1.png'
+// import card2 from '../images/card2.png'
+// import card3 from '../images/card3.png'
+// import card4 from '../images/cardm4.png'
+// import card5 from '../images/card5.png'
+// import card6 from '../images/card6.png'
 import { useState } from "react"
-// import {carDetails} from "./data"
 
-// console.log(carDetails)
-
-// const carDetails={
-//     "cars":[
-//         {
-//             id:1,
-//             model:"BMW X1",
-//             image:card1,
-//             fuel:"Electric",
-//                 price:"1,13,00,000"
-//         },{
-//             id:2,
-//             model:"BMW X2",
-//             image:card2,
-//             fuel:"Petrol",
-//             price:"83,00,000"
-//         },{
-//             id:3,
-//             model:"BMW X3",
-//             image:card3,
-//             fuel:"Diesel",
-//             price:"93,00,000"
-//         },{
-//             id:4,
-//             model:"BMW Xi",
-//             image:card4,
-//             fuel:"Electric",
-//             price:"1,60,00,000"
-//         },{
-//             id:5,
-//             model:"BMW X4",
-//             image:card5,
-//             fuel:"Electric",
-//             price:"1,55,00,000"
-//         },{
-//             id:6,
-//             model:"BMW X5",
-//             image:card6,
-//             fuel:"Electric",
-//             price:"1,22,00,000"
-//         },
-//     ]
-// }
-    
 const response = await fetch('./data.json')
-console.log(response)
-const newData= response.json
-console.log(newData)
+  console.log(response)
+  const newData= await response.json()
+  console.log(newData)
 
 function Body(){
-    
-
-    // function allCars(){
-    //     document.getElementById('cars').innerHTML=carDetails.cars.map((car) => {
-    //         console.log(car)
-    //       return   <Products
-    //           key={car.id}
-    //           model={car.model}
-    //           fuel={car.fuel}
-    //           price={car.price}
-    //           image={car.image}
-    //         />  
-            // can send the data as a single data --- data={car}
-    // })}
-    
-    
-    // function filter(){
-    //    const filtered= carDetails.cars.filter((car)=>{
-    //         const price = Number(car.price.replace(/,/g, ''))
-    //         console.log(price)
-    //          return price>10000000
-    //     })
-    //     console.log(filtered)
-    //     document.getElementById('cars').innerHTML=filtered.map((car)=>{
-    //        return <Products key={car.id}
-    //        model={car.model}
-    //        fuel={car.fuel}
-    //        price={car.price}
-    //        image={car.image} />
-    //     })
-    
-    // }
-    // const [data,setData]=useState(carDetails.cars)
-     const [data,setData]=useState(carDetails.cars)
-
+   
+     const [data,setData]=useState(newData.cars)
 
     function filter(){
-     const filtered=carDetails.cars.filter((car)=>{
+     const filtered=newData.cars.filter((car)=>{
        return Number(car.price.replace(/,/g,''))>10000000
      })
      console.log(filtered)
@@ -104,8 +26,8 @@ function Body(){
 
 
     function allCars(){
-        setData(carDetails.cars)
-        console.log(setData(carDetails.cars))
+        setData(newData.cars)
+        console.log(setData(newData.cars))
     }
 
 
@@ -124,8 +46,6 @@ function Body(){
 
         <div className="body">
             <div className="head">
-                {/* <button value="all" onClick={allCars} className="first">All</button> 
-                <button value={10000000} onClick={filter} className="first">Premium</button>  */}
                 <select  onChange={handleChange}>
                     <option className="first" value="all"> All</option>
                     <option value="10000000">Premium</option>
